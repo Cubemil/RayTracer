7feead092efadd16d2ac8a5924ee346e43be65d5
Petersen, Emil Justus <emiljustus.petersen@student.hs-anhalt.de>
2023-10-17 14:47:53 +0200

    added CalculatorTest.java implemented add()-method with 3 JUnit-Tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,11 +1,30 @@
+import org.example.Calculator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+// Test class for Calculator.java
 class CalculatorTest {
 
+    // simple numbers test for add()
     @Test
-    void add() {
-
+    void addSimpleTest() {
+        Calculator calc = new Calculator();
+        assertEquals(2, calc.add(1, 1));
     }
+
+    // test using bigger integers for add()
+    @Test
+    void addBigNumbersTest() {
+        Calculator calc = new Calculator();
+        assertEquals(517727833, calc.add(2115166, 515612667));
+    }
+
+    // test using negative numbers for add()
+    @Test
+    void addNegativeNumbersTest() {
+        Calculator calc = new Calculator();
+        assertEquals(-202715, calc.add(1337, -204052));
+    }
+
 }
