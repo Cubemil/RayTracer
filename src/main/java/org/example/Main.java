f1b6475944c72804548c1337416df0ddc11e1d57
Petersen, Emil Justus <emiljustus.petersen@student.hs-anhalt.de>
2023-10-17 14:27:47 +0200

    added sout line

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Main Branch created!");
+        System.out.println("Endlich geht der Spaß hier!");
     }
 }
