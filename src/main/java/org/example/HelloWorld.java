7bf0bd097f02ce59eb8c0925bff426f75221858a
Petersen, Emil Justus <emiljustus.petersen@student.hs-anhalt.de>
2023-10-17 14:33:49 +0200

    HelloWorld compiles

---
+++ src/main/java/org/example/HelloWorld.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Einrichten des Projekts
+        // → Hello World Programm kompiliert erfolgreich
+        System.out.println("Hello World!");
+    }
+}
