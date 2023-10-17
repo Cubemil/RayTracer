81bfab63b6af6d22ac6461cfb19bb5a85e020fc3
Petersen, Emil Justus <emiljustus.petersen@student.hs-anhalt.de>
2023-10-17 20:04:42 +0200

    Completed Point and Vector math functions

--- src/main/java/org/example/Point.java
+++ src/main/java/org/example/Point.java
@@ -13,6 +13,69 @@ public class Point {
         this.z = z;
     }
 
+    // Punkt - Punkt -> Vektor!!!
+    // Komponentenweise Subtraktion
+    public static Vector subtract(Point p1, Point p2) {
+        return new Vector(p1.x - p2.x, p1.y - p2.y, p1.z - p2.z);
+    }
 
+    // Punkt * Skalar -> Punkt
+    // Komponentenweise Multiplikation mit dem Skalar
+    public static Point multiply(Point p, double s) {
+        return new Point(p.x * s, p.y * s, p.z * s);
+    }
 
+    // Skalar * Punkt -> Punkt
+    // Komponentenweise Multiplikation mit dem Skalar
+    public static Point multiply(double s, Point p) {
+        return new Point(p.x * s, p.y * s, p.z * s);
+    }
+
+    // Punkt / Skalar -> Punkt
+    // Komponentenweise Division mit dem Skalar
+    public static Point divide(Point p, double s) {
+        return new Point(p.x / s, p.y / s, p.z / s);
+    }
+
+    // Punkt + Vektor -> Vektor
+    // Komponentenweise Addition
+    public static Point add(Point p, Vector v) {
+        return new Point(p.x + v.getX(), p.y + v.getY(), p.z + v.getZ());
+    }
+
+    // Punkt - Vektor -> Vektor
+    // Komponentenweise Subtraktion
+    public static Point subtract(Point p, Vector v) {
+        return new Point(p.x - v.getX(), p.y - v.getY(), p.z - v.getZ());
+    }
+
+    // Punkt == Punkt und Punkt != Punkt -> Boolean
+    // Komponentenweiser Vergleich
+    public static boolean equals(Point p1, Point p2) {
+        return (p1.x == p2.x &&
+                p1.y == p2.y &&
+                p1.z == p2.z);
+    }
+
+    // Komponentenweises Minimum
+    public static Point min(Point p1, Point p2) {
+        return ((p1.x < p2.x) && (p1.y < p2.y) && (p1.z < p1.z)) ? p1 : p2;
+    }
+
+    // Komponentenweises Maximum
+    public static Point max(Point p1, Point p2) {
+        return ((p1.x > p2.x) && (p1.y > p2.y) && (p1.z > p1.z)) ? p1 : p2;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
 }
--- src/main/java/org/example/Vector.java
+++ src/main/java/org/example/Vector.java
@@ -31,32 +31,31 @@ public class Vector {
         return new Vector(-v.x, -v.y, -v.z);
     }
 
-    // Hilfsmethode → Gibt den Skalar des Vektors zurück
-    private static double scalar(Vector v) {
-        return v.x + v.y + v.z;
-    }
-
-    // Skalarprodukt zweier Vektoren → Double
-    public static double dot(Vector v1, Vector v2) {
-        return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
-    }
-
     // Skalar * Vektor -> Vektor
     // Komponentenweise Multiplikation mit dem Skalar
-    public static Vector scalarTimesVector(Vector v) {
+    public static Vector multiply(double s, Vector v) {
         return new Vector(
-            scalar(v) * v.x,
-            scalar(v) * v.y,
-            scalar(v) * v.z);
+            s * v.x,
+            s * v.y,
+            s * v.z);
+    }
+
+    // Vektor * Skalar -> Vektor
+    // Komponentenweise Multiplikation mit dem Skalar
+    public static Vector multiply(Vector v, double s) {
+        return new Vector(
+                s * v.x,
+                s * v.y,
+                s * v.z);
     }
 
     // Skalar / Vektor -> Vektor
     // Komponentenweise Multiplikation mit dem Skalar
-    public static Vector scalarDividedByVector(Vector v) {
+    public static Vector divide(Vector v, double s) {
         return new Vector(
-                scalar(v) / v.x,
-                scalar(v) / v.y,
-                scalar(v) / v.z);
+                v.x / s,
+                v.y / s,
+                v.z / s);
     }
 
     // Vektor == Vektor und Vektor != Vektor -> Boolean
@@ -98,12 +97,29 @@ public class Vector {
                 vector.z / length);
     }
 
+    // Skalarprodukt zweier Vektoren → Double
+    public static double dot(Vector v1, Vector v2) {
+        return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+    }
+
     // Kreuzprodukt zweier Vektoren → Vektor
     public static Vector cross(Vector v1, Vector v2) {
         return new Vector(
                 v1.y * v2.z - v1.z * v2.y,
                 v1.z * v2.x - v1.x * v2.z,
                 v1.x * v2.y - v1.y * v2.x);
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
     }
 
 }
